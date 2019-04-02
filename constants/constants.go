e283f2e2216b3c532d16ffc7014ab388c74ee5e8
Bogdan <bogdan.utanu@gmail.com>
2019-04-02 08:42:28 +0100

    Add constants

---
+++ constants/constants.go
@@ -0,0 +1,8 @@
+package constants
+
+const (
+	// SetlistFmAPIKeyKey ...
+	SetlistFmAPIKeyKey = "SetlistFmAPIKey"
+	// SpotifyOauthTokenFileKey ...
+	SpotifyOauthTokenFileKey = "SpotifyOauthTokenFile"
+)
