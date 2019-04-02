1227fd371aa99df84546204db7f0531b8c09dcf9
Bogdan <bogdan.utanu@gmail.com>
2019-04-02 08:28:07 +0100

    Add Cobra

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
-import "github.com/setplaylistbuilder/builder"
+import "github.com/setplaylistbuilder/cmd"
 
 func main() {
-	builder.Build("kasabian")
-
+	// builder.Build("kasabian")
+	cmd.Execute()
 }
